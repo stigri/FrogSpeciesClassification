5d32f85f255771c9ccaecceaf11b1a88b0729db6
Stine Griep <stine.griep@googlemail.com>
2019-02-25 15:59:16 -0800

    code to plot heatmap on image not running yet because correct layer not defined

--- plot_confusionmatrix.py
+++ plot_confusionmatrix.py
@@ -5,6 +5,10 @@ from keras.utils import multi_gpu_model
 from vis.utils import utils
 import sys
 import numpy as np
+import matplotlib.cm as cm
+from vis.visualization import visualize_cam, overlay
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 ## load X, y, labeltonumber
 def load_data(filename):
@@ -27,7 +31,13 @@ else:
     mode = sys.argv[1]
     resize = sys.argv[2]
 
-
+filename = 'npz/data_{}_{}.npz'.format(mode, resize)
+X, y, labeltonumber = load_data(filename)
+X = X.astype('float32') / 255
+mean = X.mean(axis = 0)
+X = X - mean
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 42, stratify = y)
+print(y_test)
 ## for tests use data_{}_{}.test.npz
 ## /home/stine/repositories
 filename = 'npz/data_{}_{}.npz'.format(mode, resize)
@@ -37,10 +47,15 @@ weights = 'frogsumimodels/Xception_genus_pad_version1.1/Xception.109.0.964.hdf5'
 with tf.device('/cpu:0'):
     model = Xception(include_top = True, weights = None, classes=len(labeltonumber))
 
-layer_idx = utils.find_layer_idx(model, 'predictions')
 
 model = multi_gpu_model(model, gpus=2)
 model.load_weights(weights)
-
+layer_idx = utils.find_layer_idx(model, 'predictions')
 model.layers[layer_idx].activation = activations.linear
 model = utils.apply_modifications(model)
+
+plt.figure()
+for idx, label in enumerate(y_test):
+    grads = visualize_cam(model, layer_idx, filter_indices = label, seed_input = X_test[idx], backprop_modifier = 'relu')
+    jet_heatmap = np.uint8(cm.jet(grads)[..., :3] * 255)
+    plt.imshow(overlay(jet_heatmap, X_test[idx]))
