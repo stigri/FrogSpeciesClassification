9adee46d48d3554f3eb1ac210067d8a3e5c399c6
Stine Griep <stine.griep@googlemail.com>
2019-02-25 14:28:43 -0800

    function load data added to be able to load X, y and labeltonumber

--- plot_confusionmatrix.py
+++ plot_confusionmatrix.py
@@ -1,7 +1,34 @@
 from keras.applications import Xception
 from keras import activations
 from vis import utils
+import sys
 
+## load X, y, labeltonumber
+def load_data(filename):
+    print('[INFO] loading data...')
+    npzfile = np.load(filename)
+    X = npzfile['X']
+    y = npzfile['y']
+    labeltonumber = npzfile['labeltonumber']
+    # print(X)
+    # print(y)
+    # print(labeltonumber)
+    return X, y, labeltonumber
+
+
+if len(sys.argv) != 3:
+    sys.stderr.write(
+        'Usage: xception.py <species> or <genus>, <pad> or <distort>\n')
+    sys.exit(1)
+else:
+    mode = sys.argv[1]
+    resize = sys.argv[2]
+
+
+## for tests use data_{}_{}.test.npz
+## /home/stine/repositories
+filename = 'npz/data_{}_{}.npz'.format(mode, resize)
+X, y, labeltonumber = load_data(filename)
 
 weights = 'frogsumimodels/Xception_genus_pad_version1.1/Xception.109.0.964.hdf5'
 model = Xception(weights = weights, include_top = True)
