a5d3fa694d44a772e711d50a6aa85eae1ad496d3
Stine Griep <stine.griep@googlemail.com>
2019-03-04 14:36:31 -0800

    parameter added to chose between save heatmaps and show heatmaps. Show heatmaps does not work yet

--- plot_heatmap.py
+++ plot_heatmap.py
@@ -29,79 +29,97 @@ import keras.backend as K
 #     return X, y, labeltonumber
 
 ## function to load images img_attr containing one image of each class of test set
-def load_img_attr_data(filename):
-    print('[INFO] loading data...')
-    npzfile = np.load(filename)
+def load_img_attr_data(images):
+    print('[INFO] loading images...')
+    npzfile = np.load(images)
     img_attr = npzfile['img_attr']
     return img_attr
 
+def load_img_heatmaps(heatmaps):
+    print('[INFO] loading heatmaps...')
+    npzfile = np.load(heatmaps)
+    img_heatmaps = npzfile['img_heatmap']
+    print(img_heatmaps.shape)
+    return img_heatmaps
+
 
 ## reads parameter which describe if species or genera images are needed
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     sys.stderr.write(
-        'Usage: xception.py <species> or <genus>, <pad> or <distort>\n')
+        'Usage: plot_heatmap.py <species> or <genus>, <pad> or <distort>, <save> or <show>\n')
     sys.exit(1)
 else:
     mode = sys.argv[1]
     resize = sys.argv[2]
+    modus = sys.argv[3]
 
-filename = 'npz/img_attr_{}_{}.npz'.format(mode, resize)
-img_attr = load_img_attr_data(filename)
+images = 'npz/img_attr_{}_{}.npz'.format(mode, resize)
+heatmaps = 'img_heatmap_{}_{}.npz'.format(mode, resize)
+img_attr = load_img_attr_data(images)
 
-## normalizes images used for attribution
-norm_img = img_attr.astype('float32') / 255
-mean = img_attr.mean(axis=0)
-norm_img = img_attr - mean
+if modus == 'save':
+    ## normalizes images used for attribution
+    norm_img = img_attr.astype('float32') / 255
+    mean = img_attr.mean(axis=0)
+    norm_img = img_attr - mean
 
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 42, stratify = y)
-# print(y_test)
-## for tests use data_{}_{}.test.npz
-## /home/stine/repositories
-# filename = 'npz/img_attr_{}_{}.npz'.format(mode, resize)
-# img_attr = load_img_attr_data(filename)
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 42, stratify = y)
+    # print(y_test)
+    ## for tests use data_{}_{}.test.npz
+    ## /home/stine/repositories
+    # filename = 'npz/img_attr_{}_{}.npz'.format(mode, resize)
+    # img_attr = load_img_attr_data(filename)
 
-## creates model used for training and loads weights
-print('[INFO] create model and load weights ...')
-weights = 'Xception.100.0.941.hdf5'
-# with tf.device('/cpu:0'):
-model = Xception(include_top=True, weights=weights, classes=len(img_attr))
+    ## creates model used for training and loads weights
+    print('[INFO] create model and load weights ...')
+    weights = 'Xception.100.0.941.hdf5'
+    # with tf.device('/cpu:0'):
+    model = Xception(include_top=True, weights=weights, classes=len(img_attr))
 
-# model = multi_gpu_model(model, gpus=2)
-# model.load_weights(weights)
+    # model = multi_gpu_model(model, gpus=2)
+    # model.load_weights(weights)
 
-## changes last layer activation from softmax to linear to improve attribution results
-print('[INFO] change activations of last layer to linear ...')
-layer_idx = utils.find_layer_idx(model, 'predictions')
-model.layers[layer_idx].activation = activations.linear
-model = utils.apply_modifications(model)
+    ## changes last layer activation from softmax to linear to improve attribution results
+    print('[INFO] change activations of last layer to linear ...')
+    layer_idx = utils.find_layer_idx(model, 'predictions')
+    model.layers[layer_idx].activation = activations.linear
+    model = utils.apply_modifications(model)
 
-print(model.summary())
-print(model.input)
+    print(model.summary())
+    print(model.input)
 
-print('[INFO] start plotting ...')
-## defines last convolutional layer before dense layers
-penultimate_layer = utils.find_layer_idx(model, 'block14_sepconv2')
-plt.figure()
+    print('[INFO] start plotting ...')
+    ## defines last convolutional layer before dense layers
+    penultimate_layer = utils.find_layer_idx(model, 'block14_sepconv2')
+    plt.figure()
 
-## iterates over all images in array
-for idx, img in enumerate(norm_img):
-    print('attr image shape: {}'.format(img_attr))
-    # plt.imshow(img)
-    # plt.show()
-    # img = np.expand_dims(img, axis = 0)
-    print('norm image shape: {}'.format(img.shape))
-    ## generates a gradient based class activation map (grad-CAM) that maximizes the outputs of filter_indices in layer_idx
-    ## returns the heatmap image indicating the input regions whose change would most contribute towards maximizing the output of filter_indices
-    grads = visualize_cam(model, layer_idx, filter_indices=idx, seed_input=img, backprop_modifier='relu')
-    print('grads shape: {}'.format(grads.shape))
-    ## overlays heatmap onto original image
-    jet_heatmap = np.uint8(cm.jet(grads)[..., :3] * 255)
-    print('heatmap shape: {}'.format(jet_heatmap.shape))
-    plt.imshow(overlay(jet_heatmap, img))
-    plt.show()
-    ## creates empty array to store heatmaps
-    img_heatmap = np.ndarray(shape=(len(img_attr), 299, 299, 1), dtype=int)
-    img_heatmap[idx] = jet_heatmap
+    ## iterates over all images in array
+    for idx, img in enumerate(norm_img):
+        print('attr image shape: {}'.format(img_attr))
+        # plt.imshow(img)
+        # plt.show()
+        # img = np.expand_dims(img, axis = 0)
+        print('norm image shape: {}'.format(img.shape))
+        ## generates a gradient based class activation map (grad-CAM) that maximizes the outputs of filter_indices in layer_idx
+        ## returns the heatmap image indicating the input regions whose change would most contribute towards maximizing the output of filter_indices
+        grads = visualize_cam(model, layer_idx, filter_indices=idx, seed_input=img, backprop_modifier='relu')
+        print('grads shape: {}'.format(grads.shape))
+        ## overlays heatmap onto original image
+        # jet_heatmap = np.uint8(cm.jet(grads)[..., :3] * 255)
+        # print('heatmap shape: {}'.format(jet_heatmap.shape))
+        # plt.imshow(overlay(jet_heatmap, img))
+        # plt.show()
+        ## creates empty array to store heatmaps
+        img_heatmap = np.ndarray(shape=(len(img_attr), 299, 299, 3), dtype=int)
+        img_heatmap[idx] = grads
 
-## saves heatmap array as .npz file
-np.savez_compressed('img_heatmap_{}_{}'.format(mode, resize), img_heatmap=img_heatmap)
+    ## saves heatmap array as .npz file
+    np.savez_compressed('img_heatmap_{}_{}'.format(mode, resize), img_heatmap=img_heatmap)
+
+elif modus == 'show':
+    img_heatmaps = load_img_heatmaps(heatmaps)
+    for idx, img in enumerate(img_attr):
+        jet_heatmap = np.uint8(cm.jet(img_heatmaps[idx])[..., :3] * 255)
+        plt.imshow(jet_heatmap)
+        plt.show()
+
