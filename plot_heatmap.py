c72c2ac0bb8b390661d840739db580a0ea8a4122
Stine Griep <stine.griep@googlemail.com>
2019-12-03 12:10:53 -0800

    calculates and visualized gradcam for attribution. Saves heatmao images in npz files to oberlay with original images

--- plot_heatmap.py
+++ plot_heatmap.py
@@ -1,5 +1,5 @@
 ## original code to cite: https://github.com/raghakot/keras-vis/blob/master/examples/resnet/attention.ipynb
-## reads img_attr, calculates and visualizes cam for attribution. Saves cam heatmap images in .npz file to overlay with original images
+## reads img_attr, calculates and visualizes gradcam for attribution. Saves cam heatmap images in .npz file to overlay with original images
 ## code only runs if model is trained on cpu or single gpu. If trained parallely on multiole gpus it does not run.
 
 import tensorflow as tf
@@ -33,12 +33,14 @@ def load_all_data(filename):
     # print(labeltonumber)
     return X, y, labeltonumber
 
+
 ## function to load images img_attr containing one image of each class of test set
 def load_img_attr_data(images):
     print('[INFO] loading images...')
     npzfile = np.load(images)
     img_attr = npzfile['img_attr']
     return img_attr
+
 
 def load_img_heatmaps(heatmaps):
     print('[INFO] loading heatmaps...')
@@ -63,14 +65,14 @@ else:
 path_weights = 'frogsumimodels/Xception_{}_{}_{}/'.format(mode, resize, version)
 # images = 'npz/img_attr_{}_{}.npz'.format(mode, resize)
 images = 'npz/data_{}_{}.npz'.format(mode, resize)
-heatmaps = 'frogsumimodels/Xception_{}_{}_{}/img_heatmap_{}_{}_{}.npz'.format(mode, resize, version, mode, resize, version)
+heatmaps = 'npz/img_heatmap_{}_{}_{}.npz'.format(mode, resize, version, mode, resize, version)
 # img = load_img_attr_data(images)
 X, y, labeltonumber = load_all_data(images)
 
 path_results = 'frogsumimodels/Xception_{}_{}_{}/test_{}_{}_{}.pkl'.format(mode, resize, version, mode, resize, version)
 
 with open(path_results, 'rb') as f:
-    classreport, cnf_matrix, math_corrcoef, y_prob, y_pred = pickle.load(f)
+    accuracy, classreport, cnf_matrix, math_corrcoef, y_prob, y_pred = pickle.load(f)
 
 ## normalizes images used for attribution
 X = X.astype('float32') / 255
@@ -118,7 +120,8 @@ if modus == 'save':
         print('norm image shape: {}'.format(X_test_mean[idx].shape))
         ## generates a gradient based class activation map (grad-CAM) that maximizes the outputs of filter_indices in layer_idx
         ## returns the heatmap image indicating the input regions whose change would most contribute towards maximizing the output of filter_indices
-        grads = visualize_cam(model, layer_idx, filter_indices=y_test[idx], seed_input=X_test_mean[idx], backprop_modifier='relu')
+        grads = visualize_cam(model, layer_idx, filter_indices=y_test[idx], seed_input=X_test_mean[idx],
+                              backprop_modifier='relu')
         print('grads shape: {}'.format(grads.shape))
         img_heatmap.append(grads)
 
@@ -126,7 +129,24 @@ if modus == 'save':
     np.savez_compressed('img_heatmap_{}_{}_{}'.format(mode, resize, version), img_heatmap=img_heatmap)
 
 elif modus == 'show':
+    ## code to show all images that were misclassified (comment if you want to see heatmaps instead)
+    # for idx, img in enumerate(X_test):
+    #     if y_pred[idx] != y_test[idx]:
+    #         label = labeltonumber[y_test[idx]]
+    #         pred_label = labeltonumber[y_pred[idx]]
+    #         prob = heapq.nlargest(3, zip(y_prob[idx], labeltonumber))
+    #         first = prob[0]
+    #         second = prob[1]
+    #         third = prob[2]
+    #         fig, ax = plt.subplots(1, 1)
+    #         ax.imshow(img)
+    #         fig.suptitle('{}\n{}: {}\n{}: {}\n{}: {}'.format(label, first[1], first[0], second[1], second[0], third[1],
+    #                                                          third[0]))
+    #         plt.show()
+
+    ## code to show heatmaps (comment if you want to see misclassified images instead)
     img_heatmaps = load_img_heatmaps(heatmaps)
+    print(len(img_heatmaps))
     for idx, img in enumerate(X_test):
         label = labeltonumber[y_test[idx]]
         # pred_label = labeltonumber[y_pred[idx]]
@@ -135,10 +155,15 @@ elif modus == 'show':
         second = prob[1]
         third = prob[2]
         heatmap = img_heatmaps[idx]
-        fig, ax = plt.subplots(1, 2)
-        ax[0].imshow(img)
-        ax[1].imshow(overlay(img, heatmap))
-        fig.suptitle('{}\n{}: {}\n{}: {}\n{}: {}'.format(label, first[1], first[0] , second[1], second[0], third[1], third[0]))
-        plt.show()
+        # fig, ax = plt.subplots(1, 2)
+        # ax[0].imshow(img)
+        # ax[1].imshow(overlay(heatmap, img))
+        if label == 'Ingerophrynus':
+            fig = plt.figure()
+            im1 = plt.imshow(img)
+            im2 = plt.imshow(heatmap, alpha=0.5)
+            # fig.suptitle('{}\n{}: {}\n{}: {}\n{}: {}'.format(label, first[1], first[0] , second[1], second[0], third[1], third[0]))
+            print('{}\n{}: {}\n{}: {}\n{}: {}'.format(label, first[1], first[0], second[1], second[0], third[1],
+                                                      third[0]))
 
-
+            plt.show()
